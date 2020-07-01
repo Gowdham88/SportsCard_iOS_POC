dd955f43bda43f72d6a65e2e583687e0e5db8bc9
Gowdham_K <gowdhaman@czsm.co.in>
2020-07-01 11:40:20 +0530

    Initial Commit

---
+++ newPangestureforSportsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newPangestureforSportsApp
+//
+//  Created by Superpower on 01/07/20.
+//  Copyright © 2020 iMac superpower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
